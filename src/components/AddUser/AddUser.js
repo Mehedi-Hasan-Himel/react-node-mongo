d42a017727735148d615aa9beabf49648392a46d
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-03 08:12:04 +0600

    code

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const AddUser = () => {
-   return (
-      <div>
-         Please add a user
-      </div>
-   );
+  const handleAddUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const user = { name, email };
+
+    // send data t the server
+    fetch("http://localhost:5000/user", {
+      method: "POST",
+      headers: {
+        "content-type:": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success", data);
+      });
+  };
+  return (
+    <div>
+      <h2>Please add a user</h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Name" required />
+        <br />
+        <input type="email" name="email" placeholder="email" required />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </div>
+  );
 };
 
 export default AddUser;
