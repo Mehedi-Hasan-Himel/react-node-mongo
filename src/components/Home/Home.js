72225e828e8559e8d859699d048b733490c95ef3
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-04 02:57:36 +0600

    Delete a user from the database

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,6 +12,18 @@ const Home = () => {
     const proceed = window.confirm("Are you sure you want delete?");
     if (proceed) {
       console.log("deleting use with id, ", id);
+      const url = `http://localhost:5000/user/${id}`;
+      fetch(url, {
+        method: `DELETE`,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            console.log(`deleted`);
+             const remaining = users.filter((user) => user._id !== id);
+             setUsers(remaining)
+          }
+        });
     }
   };
 
