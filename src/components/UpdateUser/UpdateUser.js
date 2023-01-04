9e658b85d88e5cec4db34803bad6bb28da7863c2
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-04 21:08:12 +0600

    update data front end to backend

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateUser = () => {
-   const { id } = useParams()
-   return (
-      <div>
-         <h2>Updating User: {id}</h2>
-      </div>
-   );
+  const { id } = useParams();
+
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const url = `http://localhost:5000/user/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+
+  const handleUpdateUser = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+
+    const user = { name, email };
+
+    // send data to the server
+    fetch(`http://localhost:5000/user/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Success", data);
+        alert("users added successfully !!!");
+        event.target.reset();
+      });
+  };
+
+  return (
+    <div>
+      <h2>Updating User: {user.name}</h2>
+      <form onSubmit={handleUpdateUser}>
+        <input type="text" name="name" placeholder="Name" required />
+        <br />
+        <input type="email" name="email" placeholder="Email" required />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
+    </div>
+  );
 };
 
 export default UpdateUser;
